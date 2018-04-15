007ce37cdac4f50a9184c14717a566870534af52
Gabrielle Trotter <gtrotter@bulogics.com>
2018-04-15 14:18:19 -0700

    Housekeeping (cleanup)

--- src/main.c
+++ src/main.c
@@ -36,26 +36,33 @@ int core(void)
 {
     CPU_PRESCALE(0x01);
     TCNT1 = 63974;   // for 1 sec at 16 MHz  (LIE)
-    usb_init();
 
-    //TCCR1A = 0x00;
     TCCR1B = (1<<CS10) | (1<<CS12);  // Timer mode with 1024 prescaler
     TIMSK1 = (1 << TOIE1);   // Enable timer1 overflow interrupt(TOIE1)
-    sei();
-
     DDRD |= 0xFF;
-    //DDRC |= (1<<6);
     DDRB |= (1<<7);
 
-    Lights_Init(2);
+    usb_init();
+    i2c_init();
+
+    sei();
+
     _delay_ms(2000); //usb debug needs a second or two to initialize (annoying)
     print_P("usb debug print enabled\n");
+
+    Lights_Init(2);
+
     while (1)
     {
+        /*LIGHTS*/
+        //Set neopixel output to current color
         Lights_SetColor(0, ColorList[ColorIndex[0]*3], ColorList[(ColorIndex[0]*3) + 1], ColorList[(ColorIndex[0]*3)+ 2]);
         Lights_SetColor(1, ColorList[ColorIndex[1]*3], ColorList[(ColorIndex[1]*3) + 1], ColorList[(ColorIndex[1]*3)+ 2]);
-        //Lights_SetColor(0, ORANGE);
         Lights_LightOutput();
+
+        /*ACCELEROMETER*/
+        //accelerometer code will go here
+
     }
     return 0;
 }
@@ -65,7 +72,6 @@ ISR (TIMER1_OVF_vect)    // Timer1 ISR
     uint8_t j = 0;
     PORTD ^= (0xFF);
     TCNT1 = 63974;   // for 1 sec at 16 MHz (LIE)
-
     if (i == 3)
     {
         for (j = 0; j < sizeof(ColorIndex); j++)
